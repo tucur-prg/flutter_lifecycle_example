2d468e35cd6dadad8604fd5718464d5580aab615
tucur-prg <tucur.prg+github@gmail.com>
2024-09-26 16:25:17 +0900

    Universal Links Lifecicle確認

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # flutter_lifecycle_example
+
+シミュレータにコマンドでUniversal Linksを確認
+```
+xcrun simctl openurl booted "https://tucur-prg.github.io/app/link?key1=abc&key2=123"
+```
+
+CDNにデータ入ってるか確認
+```
+curl -v https://app-site-association.cdn-apple.com/a/v1/tucur-prg.github.io
+```
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,7 +7,28 @@ import UIKit
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    NSLog("LOGGING: NSLog: application(:didFinishLaunchingWithOptions)")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+  }
+
+  override func application(
+    _ application: UIApplication,
+    continue continueUserActivity: NSUserActivity,
+    restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void
+  ) -> Bool {
+    NSLog("LOGGING: NSLog: application(:continue, :restorationHandler)")
+    if continueUserActivity.activityType == NSUserActivityTypeBrowsingWeb {
+      if let url = continueUserActivity.webpageURL {
+        NSLog("LOGGING: %@", url.absoluteString)
+      } else {
+        NSLog("LOGGING: url = null")
+      }
+    }
+
+    Thread.sleep(forTimeInterval: 3.0)
+    NSLog("LOGGING: thead.sleep")
+
+    return super.application(application, continue: continueUserActivity, restorationHandler: restorationHandler)
   }
 }
